193ab4797d926b550cd61673ee6e2f4865992dc8
PauloBittencourt <pbittencourtjr@gmail.com>
2021-10-13 20:13:21 -0300

    Add Deposit and Withdraw

--- src/index.js
+++ src/index.js
@@ -60,4 +60,53 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
     return response.json(customer.statement)
 })
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body
+
+    const { customer } = request
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+    return response.status(201).send()
+})
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body
+    const { customer } = request
+
+    const balance = getBalance(customer.statement)
+
+    if(balance < amount){
+        return response.status(400).json({error: "Insufficient funds!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation)
+    return response.status(201).send()
+})
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request
+    const { date } = request.body
+
+    const dateFormat = new Date(date + "00:00")
+
+    const statement = customer.statement.filter(
+        (statement) => statement.created_at.toDateString
+    )
+
+    return response.json(customer.statement)
+})
+
 app.listen(3333)
